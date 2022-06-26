aeac62cde13d41642bfc1ca837eb852bc9ab9b22
kevin <kevinkerjaan@gmail.com>
2022-06-27 01:17:21 +0800

    complete login UI

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -5,6 +5,8 @@ import { useDispatch } from 'react-redux';
 import { setCredentials } from './authSlice';
 import { useLoginMutation } from './authApiSlice';
 
+import Logo from '../../assets/VBM-Logo.png';
+
 const Login = () => {
     const userRef = useRef()
     const errRef = useRef()
@@ -16,13 +18,13 @@ const Login = () => {
     const [login, { isLoading }] = useLoginMutation()
     const dispatch = useDispatch()
 
-    useEffect(() => {
-      userRef.current.focus()
-    },[]);
+    // useEffect(() => {
+    //   userRef.current.focus()
+    // },[]);
 
-    useEffect(() => {
-      setErrMsg('')
-    },[user,pwd]);
+    // useEffect(() => {
+    //   setErrMsg('')
+    // },[user,pwd]);
 
     const handleSubmit = async (e) => {
       e.preventDefault();
@@ -55,12 +57,36 @@ const Login = () => {
     const handlePwdInput = (e) => setPwd(e.target.value)
 
     const content = isLoading ? <h1>Loading...</h1> : (
-        <section className="login flex">
+        <section className="login flex direction">
           <div className="left-side">
-            <h3>“Without hard work, nothing grows but weeds.”</h3>
+            <div className="bg-img">
+              <div className="container">
+                <h3 className="fs-600">“Without hard work, nothing<br/>grows but weeds.”</h3>
+                <h6 className="fs-400">- Gordon B. Hinckley</h6>
+              </div>
+            </div>
           </div>
           <div className="right-side">
-
+            <div className="container flow">
+              <div className="pl-7">
+                <img src={Logo} alt="logo"/>
+                <h1 className="fs-700">Welcome to<br/><span className="uppercase">property</span> Admin Panel</h1>
+                <h6>Sign in to your account below</h6>
+                <form className="flow">
+                  <div className="form-set flow flow-space--small">
+                    <label>Email</label>
+                    <input className="input-box" type="email" placeholder="Type your email here" />
+                  </div>
+                  <div className="form-set flow flow-space--small">
+                    <label>Password</label>
+                    <i className="icon show-password"></i>
+                    <input className="input-box" type="password" placeholder="Type your password here" />
+                  </div>
+                  <input type="submit" className="button-orange" value="Sign in"/>
+                </form>
+                <p>Version 1.0.0</p>
+              </div>
+            </div>
           </div>
             {/* <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
 
